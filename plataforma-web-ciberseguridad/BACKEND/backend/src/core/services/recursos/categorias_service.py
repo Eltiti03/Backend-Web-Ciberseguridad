c5736d1dfe18b04226e0baf4c0fdefdce1a1d894
Julian Gutierrez <juligumer03@gmail.com>
2026-03-31 16:51:40 -0500

    Aplique el CORS para que deje consultar los endpoints del backend en el frontend

--- plataforma-web-ciberseguridad/BACKEND/backend/src/core/services/recursos/categorias_service.py
+++ plataforma-web-ciberseguridad/BACKEND/backend/src/core/services/recursos/categorias_service.py
@@ -1,6 +1,23 @@
 from ...models.categoria_recurso import CategoriaRecurso
+from ...models import CategoriaRecurso, RecursoEducativo
 
 def all_categorias_recursos():
     
     result = CategoriaRecurso.objects.all()
     return result
+
+def crear_nuevo_recurso(titulo, descripcion, url_recurso, tipo_recurso, es_publico, categoria_id=None):
+    categoria = None
+    if categoria_id:
+        # Si la categoría no existe, dejamos que levante el error para que el controlador lo maneje
+        categoria = CategoriaRecurso.objects.get(categoria_id=categoria_id)
+
+    recurso = RecursoEducativo.objects.create(
+        titulo=titulo,
+        descripcion=descripcion,
+        url_recurso=url_recurso,
+        tipo_recurso=tipo_recurso,
+        es_publico=es_publico,
+        categoria=categoria
+    )
+    return recurso
