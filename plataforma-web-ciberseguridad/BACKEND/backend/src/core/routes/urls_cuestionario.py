e99e2de32bbea86a6f5a761ea0fbb46324a3a0eb
Daniel Pino <dajpei@gmail.com>
2026-04-18 11:53:14 -0500

    se registra ruta editar cuestionario

--- plataforma-web-ciberseguridad/BACKEND/backend/src/core/routes/urls_cuestionario.py
+++ plataforma-web-ciberseguridad/BACKEND/backend/src/core/routes/urls_cuestionario.py
@@ -1,18 +1,32 @@
 from django.urls import path
-
-from ..controllers.views_cuestionarios import crear_cuestionario, eliminar_cuestionario, iniciar_intento, obtener_cuestionarios, crear_pregunta, editar_pregunta,eliminar_pregunta,obtener_preguntas_cuestionario, crear_opcion, editar_opcion, eliminar_opcion, responder_pregunta
+from ..controllers.views_cuestionarios import (
+    crear_cuestionario,
+    editar_cuestionario,
+    eliminar_cuestionario,
+    iniciar_intento,
+    obtener_cuestionarios,
+    crear_pregunta,
+    editar_pregunta,
+    eliminar_pregunta,
+    obtener_preguntas_cuestionario,
+    crear_opcion,
+    editar_opcion,
+    eliminar_opcion,
+    responder_pregunta,
+)
 
 urlpatterns = [
-    path('cuestionario/crear/', crear_cuestionario),
-    path('cuestionario/obtener/all/', obtener_cuestionarios),
-    path('cuestionario/eliminar/<uuid:cuestionario_id>/', eliminar_cuestionario),
-    path('cuestionario/pregunta/crear/', crear_pregunta ),
-    path('cuestionario/pregunta/editar/<uuid:pregunta_id>/', editar_pregunta),
-    path('cuestionario/pregunta/eliminar/<uuid:pregunta_id>/', eliminar_pregunta),
-    path('cuestionario/pregunta/obtener/<uuid:cuestionario_id>/', obtener_preguntas_cuestionario),
-    path('cuestionario/pregunta/opcion/crear/', crear_opcion),
-    path('cuestionario/pregunta/opcion/editar/<uuid:opcion_id>/', editar_opcion),
-    path('cuestionario/pregunta/opcion/eliminar/<uuid:opcion_id>/', eliminar_opcion),
-    path('cuestionario/intento/iniciar/', iniciar_intento),
-    path('cuestionario/intento/responder/', responder_pregunta),
+    path('cuestionario/crear/',                                    crear_cuestionario),
+    path('cuestionario/obtener/all/',                              obtener_cuestionarios),
+    path('cuestionario/editar/<uuid:cuestionario_id>/',            editar_cuestionario),
+    path('cuestionario/eliminar/<uuid:cuestionario_id>/',          eliminar_cuestionario),
+    path('cuestionario/pregunta/crear/',                           crear_pregunta),
+    path('cuestionario/pregunta/editar/<uuid:pregunta_id>/',       editar_pregunta),
+    path('cuestionario/pregunta/eliminar/<uuid:pregunta_id>/',     eliminar_pregunta),
+    path('cuestionario/pregunta/obtener/<uuid:cuestionario_id>/',  obtener_preguntas_cuestionario),
+    path('cuestionario/pregunta/opcion/crear/',                    crear_opcion),
+    path('cuestionario/pregunta/opcion/editar/<uuid:opcion_id>/',  editar_opcion),
+    path('cuestionario/pregunta/opcion/eliminar/<uuid:opcion_id>/',eliminar_opcion),
+    path('cuestionario/intento/iniciar/',                          iniciar_intento),
+    path('cuestionario/intento/responder/',                        responder_pregunta),
 ]
